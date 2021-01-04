0512c95f5cc62a129b3cb7f48b1a5a2e9b55000d
Sebastian Dittrich <sebastian.dittrich@live.de>
2021-01-04 21:53:52 +0100

    Updated opaque version

--- src/LocalAdapter.ts
+++ src/LocalAdapter.ts
@@ -1,41 +1,41 @@
 import { v4 } from "uuid"
-import { OpaqueModel, ModelAttributes, IdType, RootQuery, Constructor, AdapterContract } from "@opaquejs/opaque"
+import { OpaqueModel, ModelAttributes, IdType, RootQuery, Constructor, AdapterContract, OpaqueRow } from "@opaquejs/opaque"
 import { queryCollection } from "./backend"
 
 export class LocalOpaqueAdapter<Model extends typeof OpaqueModel> implements AdapterContract {
-    storage: Map<IdType, ModelAttributes<InstanceType<Model>>> = new Map()
+    storage: Map<IdType, OpaqueRow> = new Map()
 
     constructor(public model: Model) {
     }
 
-    async delete(query: RootQuery<ModelAttributes<InstanceType<Model>>>) {
-        for (const { [this.model.primaryKey as keyof ModelAttributes<InstanceType<Model>>]: id } of queryCollection([...this.storage.values()], query)) {
+    async delete(query: RootQuery<OpaqueRow>) {
+        for (const { [this.model.primaryKey as keyof OpaqueRow]: id } of queryCollection([...this.storage.values()], query)) {
             this.storage.delete(id as IdType)
         }
     }
 
-    async create(data: ModelAttributes<InstanceType<Model>>) {
+    async create(data: OpaqueRow) {
         const obj = { ...data } as any
         if (obj[this.model.primaryKey] === undefined) {
             obj[this.model.primaryKey] = v4()
         }
         this.storage.set(obj[this.model.primaryKey], obj)
-        return obj
+        return this.storage.get(obj[this.model.primaryKey])!
     }
 
-    async update(query: RootQuery<ModelAttributes<InstanceType<Model>>>, data: ModelAttributes<InstanceType<Model>>) {
-        const result: ModelAttributes<InstanceType<Model>>[] = []
-        for (const { [this.model.primaryKey as keyof ModelAttributes<InstanceType<Model>>]: id } of await this.read(query)) {
+    async update(query: RootQuery<OpaqueRow>, data: OpaqueRow) {
+        const result: OpaqueRow[] = []
+        for (const { [this.model.primaryKey as keyof OpaqueRow]: id } of await this.read(query)) {
             const previous = this.storage.get(id as IdType)!
             result.push(previous)
             for (const key in data) {
-                (previous as any)[key] = (data as any)[key] as any
+                previous[key] = data[key]
             }
         }
         return result
     }
 
-    async read(query: RootQuery<ModelAttributes<InstanceType<Model>>>) {
+    async read(query: RootQuery<OpaqueRow>) {
         return queryCollection([...this.storage.values()], query)
     }
 }
