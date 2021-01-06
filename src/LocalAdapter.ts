6baddf7f2f315c5a57786433ec1789b8db2e63cb
Sebastian Dittrich <sebastian.dittrich@live.de>
2021-01-06 21:32:37 +0100

    Added sync adapter support

--- src/LocalAdapter.ts
+++ src/LocalAdapter.ts
@@ -1,20 +1,33 @@
 import { v4 } from "uuid"
-import { OpaqueModel, ModelAttributes, IdType, RootQuery, Constructor, AdapterContract, OpaqueRow } from "@opaquejs/opaque"
+import { OpaqueModel, IdType, RootQuery, AdapterContract, OpaqueRow, SyncAdapterContract } from "@opaquejs/opaque"
 import { queryCollection } from "./backend"
 
-export class LocalOpaqueAdapter<Model extends typeof OpaqueModel> implements AdapterContract {
+export class LocalOpaqueAdapter<Model extends typeof OpaqueModel> implements AdapterContract, SyncAdapterContract {
     storage: Map<IdType, OpaqueRow> = new Map()
 
     constructor(public model: Model) {
     }
 
+    async create(data: OpaqueRow) {
+        return this.createSync(data)
+    }
+    async update(query: RootQuery<OpaqueRow>, data: OpaqueRow) {
+        return this.updateSync(query, data)
+    }
+    async read(query: RootQuery<OpaqueRow>) {
+        return this.readSync(query)
+    }
     async delete(query: RootQuery<OpaqueRow>) {
+        return this.deleteSync(query)
+    }
+
+    deleteSync(query: RootQuery<OpaqueRow>) {
         for (const { [this.model.primaryKey as keyof OpaqueRow]: id } of queryCollection([...this.storage.values()], query)) {
             this.storage.delete(id as IdType)
         }
     }
 
-    async create(data: OpaqueRow) {
+    createSync(data: OpaqueRow) {
         const obj = { ...data } as any
         if (obj[this.model.primaryKey] === undefined) {
             obj[this.model.primaryKey] = v4()
@@ -23,9 +36,9 @@ export class LocalOpaqueAdapter<Model extends typeof OpaqueModel> implements Ada
         return this.storage.get(obj[this.model.primaryKey])!
     }
 
-    async update(query: RootQuery<OpaqueRow>, data: OpaqueRow) {
+    updateSync(query: RootQuery<OpaqueRow>, data: OpaqueRow) {
         const result: OpaqueRow[] = []
-        for (const { [this.model.primaryKey as keyof OpaqueRow]: id } of await this.read(query)) {
+        for (const { [this.model.primaryKey as keyof OpaqueRow]: id } of this.readSync(query)) {
             const previous = this.storage.get(id as IdType)!
             result.push(previous)
             for (const key in data) {
@@ -35,7 +48,7 @@ export class LocalOpaqueAdapter<Model extends typeof OpaqueModel> implements Ada
         return result
     }
 
-    async read(query: RootQuery<OpaqueRow>) {
+    readSync(query: RootQuery<OpaqueRow>) {
         return queryCollection([...this.storage.values()], query)
     }
 }
